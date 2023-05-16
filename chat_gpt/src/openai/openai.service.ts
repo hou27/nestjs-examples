c514cb961651540ee05ff379dee184f6b7d70e16
hou27 <ataj125@gmail.com>
2023-05-16 18:54:34 +0900

    feat: Add gpt-3.5-turbo model example - #8

--- chat_gpt/src/openai/openai.service.ts
+++ chat_gpt/src/openai/openai.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import {
   Configuration,
+  CreateChatCompletionResponse,
   CreateCompletionRequest,
   CreateCompletionResponse,
   OpenAIApi,
@@ -25,10 +26,34 @@ export class OpenaiService {
     model,
     temperature,
   }: CreateCompletionBodyDto): Promise<CreateCompletionResponse> {
+    console.log({ question, model, temperature });
     try {
       const { data } = await this.openAIApi.createCompletion({
         prompt: question,
         model: model || 'text-davinci-003',
+        temperature: temperature || 0.9,
+      });
+
+      return data;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async createChatCompletion({
+    question,
+    model,
+    temperature,
+  }: CreateCompletionBodyDto): Promise<CreateChatCompletionResponse> {
+    try {
+      const { data } = await this.openAIApi.createChatCompletion({
+        model: model || 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'user',
+            content: question,
+          },
+        ],
         temperature: temperature || 0.9,
       });
 
